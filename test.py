c5d15485eae1a511f33ad45b25fc8f4a20b0d327
vishal kumar yadav <vishalyadav382003@gmail.com>
2022-11-26 13:04:37 +0530

    final code push

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import string
+import random
 if __name__ == "__main__":
     s1 = string.ascii_lowercase
-    print(s1)
-    s2=string.ascii_uppercase
-    print(s1+s2)
+    s2 = string.ascii_uppercase    
+    s3 = string.digits
+    s4= string.punctuation
+    plen = int(input("Enter the password Length"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    #print(s)
+    random.shuffle(s)
+    #print(s)
+    print("".join(random.sample(s, plen)))
+
+
+
+
+
+
 
 
